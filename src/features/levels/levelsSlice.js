d3b556ae91ba07cccd699fd0ec7b8f37251589a0
umairahmed17 <umair.ahmad1257@gmail.com>
2022-04-11 14:59:29 +0500

    Added levelsSlice

---
+++ src/features/levels/levelsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  levels: [],
+};
+
+const levelsSlice = createSlice({
+  name: "levels",
+  initialState,
+  reducers: {
+    setMembershipLevels: (state, { payload }) => {
+      state.levels = payload;
+    },
+  },
+});
+
+export const { setMembershipLevels } = levelsSlice.actions;
+
+export const selectMembershipLevels = (state) => state.levels;
+
+export default levelsSlice.reducer;
